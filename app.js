a047c12d63593e5f3c0baabcc89f7898fbcf24eb
samtur <108389765+samtur@users.noreply.github.com>
2022-07-06 18:01:55 +0800

    Began using passport to implement authentication in the app.

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-// REQUIRMENTS AND CONNECTIONS
-// Here we are requiring express and path.
+// REQUIRMENTS
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-// Requiring ejs-mate
 const ejsMate = require('ejs-mate');
-// require session
-const session = require('express-session')
-const flash = require('connect-flash')
-// Requiring function from other file.
-const expressError = require('./utils/expressError')
-// Require model.
+const session = require('express-session');
+const flash = require('connect-flash');
+const expressError = require('./utils/expressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews')
+// MODELS
+const User = require('./models/user');
 
-// Require mongoose and connect.
+// ROUTE REQUIREMENTS
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users')
+
+// CONNECTIONS
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
-// Logic to check for errors on db connection.
-// db variable shortens syntax.
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-    console.log('database connected!')
+    console.log('database connected!');
 });
-// Calling express.
-const app = express();
-// Using ejs-mate
-app.engine('ejs', ejsMate)
-// Here we are linking to our views ejs templates.
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-// Here we are allowing our req.body to be parsed so
-// we can display it.
-app.use(express.urlencoded({ extended: true }))
-// We need to use method override.
-app.use(methodOverride('_method'))
-// Serving static assets
-app.use(express.static(path.join(__dirname, 'public')))
 
+// EXPRESS EXECUTION
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// METHOD OVERRIDE EXECUTION
+app.use(methodOverride('_method'));
+
+// EJS EXECUTION
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// SESSION EXECUTION/CONFIG
 const sessionConfig = {
     secret: 'thisisasecret',
     resave: false,
@@ -50,49 +50,44 @@ const sessionConfig = {
         httpOnly: true,
     }
 }
-app.use(session(sessionConfig))
-
+app.use(session(sessionConfig));
 app.use(flash());
 
+// PASSPORT EXECUTION/CONFIG
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// FLASH EXECUTION
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next()
 })
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+// ROUTES EXECUTION
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
-
-// SITE ROUTES
-// Here we are creating a basic route to test.
-// We render our home to test our home.ejs
+// MAIN ROUTE
 app.get('/', (req, res) => {
     res.render('home');
 })
 
-
-// app.all covers all requests '*' = every path.
-// The covers errors for wrong urls
-// This will only run if nothing has matched before.
-// Respond with our class, a message and error code.
+// ERROR HANDLING
 app.all('*', (req, res, next) => {
-    next(new expressError('Page not found', 404))
-})
-
-
-// This is our error handler for our app.
-// Render errors template!
-// Pass entire err through render.
-// Now message won't default so...
-// ..if statement containing default behaviour.
+    next(new expressError('Page not found', 404));
+});
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh no! You have an error!';
     res.status(statusCode).render('errors', { err });
 })
 
-// Here we are listening for our server.
+//LISTENING
 app.listen(3000, () => {
     console.log('Serving on port 3000');
 })
---
+++ routes/users.js
@@ -0,0 +1,42 @@
+// REQUIREMENTS
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const User = require('../models/user');
+const wrapAsync = require('../utils/wrapAsync');
+// REGISTER ROUTES
+router.get('/register', (req, res) => {
+    res.render('users/register');
+});
+
+router.post('/register', wrapAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Yelp Camp');
+        res.redirect('/campgrounds');
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+}));
+// LOGIN ROUTES
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/campgrounds')
+})
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        req.flash('success', 'Successfully Logged Out!');
+        res.redirect('/campgrounds');
+    });
+});
+// EXECUTING ROUTER
+module.exports = router;
